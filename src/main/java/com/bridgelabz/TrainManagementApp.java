fd1dca78b113e6050eb09dc49373b155bbec6ea8
Koti reddy <koteswara.reddy@bridgelabz.com>
2026-06-24 15:42:38 +0530

    Track Unique Bogie IDs (Set – HashSet)

--- src/main/java/com/bridgelabz/TrainManagementApp.java
+++ src/main/java/com/bridgelabz/TrainManagementApp.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -9,46 +10,27 @@ public class TrainManagementApp {
 
     public static void main(String[] args) {
 
-        System.out.println("====================");
-        System.out.println("Train Consist Management App");
-        System.out.println("====================");
+        System.out.println("===============");
+        System.out.println("UC3 - Track Unique Bogie IDs");
+        System.out.println("===============");
 
-        //Create a dynamic list to store train bogies
-        List<String> trainConsit = new ArrayList<>();
+        //Create a Set to store unique bogie ID's
+        //Hash set store only unique id's
+        HashSet<String>  bogies = new HashSet<>();
 
+        //add bogies with duplicates also
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
 
-
-        System.out.println("Train initialized successfully");
-        System.out.println("Intial Bogi count "+trainConsit.size());
-        System.out.println("Current Train Consist :"+trainConsit);
-
-
-        System.out.println("======UC2 Add Passenger Bogies to Train======");
-        List<String> passengerBhogies = new ArrayList<>();
-
-        //inserting bhogies
-        passengerBhogies.add("Sleeper class");
-        passengerBhogies.add("AC chair");
-        passengerBhogies.add("General class");
-        passengerBhogies.add("Sleeper class");
-        passengerBhogies.add("General class");
-
-        System.out.println("Displaying Bhogies after inserting :"+passengerBhogies);
-
-        //removing bhogi
-        passengerBhogies.remove("AC chair");
-        System.out.println("After removing AC chair ");
-        System.out.println("Displaying Bhogies :"+passengerBhogies);
-
-        //check Sleeper exitst;
-        System.out.println(passengerBhogies.contains("Sleeper class")? true: false);
-
-        //Final Train Passanger Consist
-        System.out.println(passengerBhogies);
-
-        System.out.println("UC2 operations completed successfully.....");
+        //Duplicate entries will be ignored internally by a Hashset
+        bogies.add("BG101");
+        bogies.add("BG102");
 
 
+        System.out.println("Bogies ID's After Insertion :");
+        System.out.println(bogies);
 
     }
 }
