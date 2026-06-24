adc2bd797f6b7c266482c5082047b427d47ba0b3
Koti reddy <koteswara.reddy@bridgelabz.com>
2026-06-24 15:32:31 +0530

    Add Passenger Bogies to Train

--- src/main/java/com/bridgelabz/TrainManagementApp.java
+++ src/main/java/com/bridgelabz/TrainManagementApp.java
@@ -21,5 +21,34 @@ public class TrainManagementApp {
         System.out.println("Train initialized successfully");
         System.out.println("Intial Bogi count "+trainConsit.size());
         System.out.println("Current Train Consist :"+trainConsit);
+
+
+        System.out.println("======UC2 Add Passenger Bogies to Train======");
+        List<String> passengerBhogies = new ArrayList<>();
+
+        //inserting bhogies
+        passengerBhogies.add("Sleeper class");
+        passengerBhogies.add("AC chair");
+        passengerBhogies.add("General class");
+        passengerBhogies.add("Sleeper class");
+        passengerBhogies.add("General class");
+
+        System.out.println("Displaying Bhogies after inserting :"+passengerBhogies);
+
+        //removing bhogi
+        passengerBhogies.remove("AC chair");
+        System.out.println("After removing AC chair ");
+        System.out.println("Displaying Bhogies :"+passengerBhogies);
+
+        //check Sleeper exitst;
+        System.out.println(passengerBhogies.contains("Sleeper class")? true: false);
+
+        //Final Train Passanger Consist
+        System.out.println(passengerBhogies);
+
+        System.out.println("UC2 operations completed successfully.....");
+
+
+
     }
 }
